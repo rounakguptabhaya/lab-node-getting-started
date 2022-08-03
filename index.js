634edc7e41aacee4d9dd742da85bfc6b07affbe9
Rounak <rounakguptabhaya@gmail,com>
2022-08-03 21:55:03 +0530

    First commit

--- index.js
+++ index.js
@@ -1,28 +1,64 @@
 class SortedList {
   constructor() {
-  
+    this.items = [];
+    this.length = 0;
   }
-  add() {
-   
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a > b ? 1 : -1);
+    this.length = this.items.length;
   }
-  get() {
-
+  get(i) {
+    if (i >= this.items.length) {
+      throw new Error('OutOfBounds')
+    }
+    return this.items[i];
   }
   max() {
-
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    else {
+      return Math.max(...this.items);
+    }
   }
   min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
 
+    else {
+      return Math.min(...this.items);
+    }
   }
   avg() {
-    
-   
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    else {
+      var s = 0;
+      this.items.forEach(function (item) {
+        s = s + item;
+      })
+      return s / this.items.length;
+    }
+
   }
 
   sum() {
-  
+    if (this.items.length === 0) {
+      return 0;
+    }
+    else {
+      var s = 0;
+      this.items.forEach(function (item) {
+        s = s + item;
+      })
+      return s;
+    }
+
   }
-  
+
 };
 
 module.exports = SortedList;
